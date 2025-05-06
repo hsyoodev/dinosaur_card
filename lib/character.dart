b0804f0de934aa8133eec2caeef630915c5e6cea
hsyoodev <hsyoodev@icloud.com>
2025-05-06 20:59:01 +0900

    앱 레이아웃 구성 이해하기

---
+++ lib/character.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class Character extends StatelessWidget {
+  const Character({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text(
+          "DINOSAUR CARD",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 120, 77, 4),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Image.asset("images/di1.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Text("Tyrannosaurus rex", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("images/di2.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Text("Brachiosaurus", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Image.asset("images/di1.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Text("Tyrannosaurus rex", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("images/di2.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Text("Brachiosaurus", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Image.asset("images/di1.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Text("Tyrannosaurus rex", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("images/di2.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Text("Brachiosaurus", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
